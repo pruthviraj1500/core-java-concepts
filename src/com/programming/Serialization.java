0577b13daace25c3b2ce0513f115613c67d73aa1
63606_Pruthviraj <pruthvirajab1008@gmail.com>
2024-09-18 16:52:15 +0530

    Add Serialization

---
+++ src/com/programming/Serialization.java
@@ -0,0 +1,51 @@
+package com.programming;
+
+import java.io.*;
+
+class Student implements Serializable {
+
+    int id;
+    String name;
+    int marks;
+
+    public Student(int id, String name, int marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+}
+
+
+public class Serialization {
+
+    public static void main(String[] args){
+        System.out.println("In main method");
+
+        try{
+
+            //Serialization
+
+            Student s1 = new Student(1,"Pruthvi",40);
+            FileOutputStream fos = new FileOutputStream("D:/pruthviraj/Core java/serialization_demo.txt");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(s1);
+            oos.flush();
+            oos.close();
+            System.out.println("Student object s1 state saved in the file successfully");
+
+            //Deserialization
+
+            FileInputStream fis = new FileInputStream("D:/pruthviraj/Core java/serialization_demo.txt");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Student s2 = (Student) ois.readObject();
+            System.out.println("Student object state retrieved from file successfully");
+            System.out.println(s2.id +" "+ s2.name +" "+ s2.marks);
+            ois.close();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+}
