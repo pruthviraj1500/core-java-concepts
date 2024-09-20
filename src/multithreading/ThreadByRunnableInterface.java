b89a273be19042c0cac9e6e794986ddabadae8c6
63606_Pruthviraj <pruthvirajab1008@gmail.com>
2024-09-20 17:52:04 +0530

    Create thread using Thread class and Runnable interface

---
+++ src/multithreading/ThreadByRunnableInterface.java
@@ -0,0 +1,30 @@
+package multithreading;
+
+class Greek{
+    public void m1(){
+        System.out.println("In m1 method of Greek class");
+    }
+}
+
+public class ThreadByRunnableInterface extends Greek implements Runnable{
+
+    public void run(){
+        System.out.println("In run method");
+    }
+
+    public static void main(String[] args){
+
+        ThreadByRunnableInterface t1 = new ThreadByRunnableInterface();
+        t1.m1();
+
+        Thread th = new Thread(t1);
+        th.start();
+
+        Thread th2 = new Thread(t1);
+        th2.start();
+
+        System.out.println("In main method");
+        
+    }
+
+}
